2d4f3db0960276ee472b307028c292788ddaf9c6
philw <philwong628@gmail.com>
2022-12-14 00:38:36 +1000

    Add new entry button and new entry form complete

--- instructor-journal/controllers/journals_controller.rb
+++ instructor-journal/controllers/journals_controller.rb
@@ -4,4 +4,24 @@ get '/' do
       journals: journals
     }
 end
-  
+
+get '/journals/new' do
+  erb :'journals/new'
+end
+
+post '/journals' do 
+
+  location = params['location']
+  date = params['date']
+  instructor_name = params['instructor_name']
+  other_instructor = params['other_instructor']
+  new_students = params['new_students']
+  injuries = params['injuries']
+  drills = params['drills']
+  notes = params['notes']
+  image_url = params['image_url']
+
+  create_journals(location, date, instructor_name, other_instructor, new_students, injuries, drills, notes, image_url)
+
+  redirect '/'
+end
--- instructor-journal/models/journal.rb
+++ instructor-journal/models/journal.rb
@@ -1,3 +1,7 @@
 def all_journals
     run_sql('SELECT * FROM journals ORDER BY id')
 end
+
+def create_journals(location, date, instructor_name, other_instructor, new_students, injuries, drills, notes, image_url)
+    run_sql("INSERT INTO journals(location, date, instructor_name, other_instructor, new_students, injuries, drills, notes, image_url) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)", [location, date, instructor_name, other_instructor, new_students, injuries, drills, notes, image_url])
+end
