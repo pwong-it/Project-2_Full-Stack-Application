f47b215d06ce07afb24afe984c42e358a6b5fe4d
philw <philwong628@gmail.com>
2022-12-14 10:02:06 +1000

    Edit post working

--- instructor-journal/controllers/journals_controller.rb
+++ instructor-journal/controllers/journals_controller.rb
@@ -21,7 +21,33 @@ post '/journals' do
   notes = params['notes']
   image_url = params['image_url']
 
-  create_journals(location, date, instructor_name, other_instructor, new_students, injuries, drills, notes, image_url)
+  create_journal(location, date, instructor_name, other_instructor, new_students, injuries, drills, notes, image_url)
+
+  redirect '/'
+end
+
+get '/journals/:id/edit' do
+  id = params['id']
+  journal = get_journal(id)
+
+  erb :'journals/edit', locals: {
+    journal: journal
+  }
+end
+
+put '/journals/:id' do
+  id = params['id']
+  location = params['location']
+  date = params['date']
+  instructor_name = params['instructor_name']
+  other_instructor = params['other_instructor']
+  new_students = params['new_students']
+  injuries = params['injuries']
+  drills = params['drills']
+  notes = params['notes']
+  image_url = params['image_url']
+
+  update_journal(id, location, date, instructor_name, other_instructor, new_students, injuries, drills, notes, image_url)
 
   redirect '/'
 end
--- instructor-journal/models/journal.rb
+++ instructor-journal/models/journal.rb
@@ -2,6 +2,14 @@ def all_journals
     run_sql('SELECT * FROM journals ORDER BY id')
 end
 
-def create_journals(location, date, instructor_name, other_instructor, new_students, injuries, drills, notes, image_url)
+def create_journal(location, date, instructor_name, other_instructor, new_students, injuries, drills, notes, image_url)
     run_sql("INSERT INTO journals(location, date, instructor_name, other_instructor, new_students, injuries, drills, notes, image_url) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)", [location, date, instructor_name, other_instructor, new_students, injuries, drills, notes, image_url])
+end
+
+def get_journal(id)
+    run_sql('SELECT * FROM journals WHERE id = $1', [id])[0]
+end
+
+def update_journal(id, location, date, instructor_name, other_instructor, new_students, injuries, drills, notes, image_url)
+    run_sql("UPDATE journals SET location = $2, date = $3, instructor_name = $4, other_instructor = $5, new_students = $6, injuries = $7, drills = $8, notes = $9, image_url = $10 WHERE id = $1", [id, location, date, instructor_name, other_instructor, new_students, injuries, drills, notes, image_url])
 end
